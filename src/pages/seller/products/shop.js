aa8f09588e7a2aa2b3e949427ebc9e1276616ab7
Sampan Verma <sampan.developer@gmail.com>
2021-06-30 11:34:14 +0530

    Added more options to shop settings page

--- src/pages/seller/products/shop.js
+++ src/pages/seller/products/shop.js
@@ -6,6 +6,7 @@ export default function SellerShop() {
       <div className="card-inner">
         <h5 className="card-title">Web Store Setting</h5>
         <p>Here is your basic store setting of your website.</p>
+
         <form action="#" className="gy-3 form-settings">
           <div className="row g-3 align-center">
             <div className="col-lg-5">
@@ -82,61 +83,118 @@ export default function SellerShop() {
           <div className="row g-3 align-center">
             <div className="col-lg-5">
               <div className="form-group">
-                <label className="form-label">Allow Registration</label>
+                <label className="form-label">Website Link</label>
                 <span className="form-note">
-                  Enable or disable registration from site.
+                  Specify the URL if your main website is external.
                 </span>
               </div>
             </div>
             <div className="col-lg-7">
-              <ul className="custom-control-group g-3 align-center flex-wrap">
-                <li>
-                  <div className="custom-control custom-radio checked">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      checked=""
-                      name="reg-public"
-                      id="reg-enable"
-                    />
-                    <label className="custom-control-label" for="reg-enable">
-                      Enable
-                    </label>
-                  </div>
-                </li>
-                <li>
-                  <div className="custom-control custom-radio">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      name="reg-public"
-                      id="reg-disable"
-                    />
-                    <label className="custom-control-label" for="reg-disable">
-                      Disable
-                    </label>
-                  </div>
-                </li>
-                <li>
-                  <div className="custom-control custom-radio">
-                    <input
-                      type="radio"
-                      className="custom-control-input"
-                      name="reg-public"
-                      id="reg-request"
-                    />
-                    <label className="custom-control-label" for="reg-request">
-                      On Request
-                    </label>
-                  </div>
-                </li>
-              </ul>
+              <div className="form-group">
+                <div className="form-control-wrap">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="site-url"
+                    value="https://www.softnio.com"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row g-3">
+            <div className="col-lg-7 offset-lg-5">
+              <div className="form-group mt-2">
+                <button type="submit" className="btn btn-lg btn-primary">
+                  Update
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <hr />
+
+        <h5 className="card-title mt-5">Store Address</h5>
+        <p>Here is your publicly accessible store address.</p>
+
+        <form action="#" className="gy-3 form-settings">
+          <div className="row g-3 align-center">
+            <div className="col-lg-5">
+              <div className="form-group">
+                <label className="form-label" for="site-name">
+                  Address Line 1
+                </label>
+                <span className="form-note">
+                  Specify the name of your website.
+                </span>
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="form-group">
+                <div className="form-control-wrap">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="site-name"
+                    value="My Store"
+                  />
+                </div>
+              </div>
             </div>
           </div>
           <div className="row g-3 align-center">
             <div className="col-lg-5">
               <div className="form-group">
-                <label className="form-label">Main Website</label>
+                <label className="form-label" for="site-email">
+                  Address Line 2
+                </label>
+                <span className="form-note">
+                  Specify the email address of your website.
+                </span>
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="form-group">
+                <div className="form-control-wrap">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="site-email"
+                    value="info@softnio.com"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row g-3 align-center">
+            <div className="col-lg-5">
+              <div className="form-group">
+                <label className="form-label" for="site-copyright">
+                  City
+                </label>
+                <span className="form-note">
+                  Copyright information of your website.
+                </span>
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="form-group">
+                <div className="form-control-wrap">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="site-copyright"
+                    value="© 2019, DashLite. All Rights Reserved."
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row g-3 align-center">
+            <div className="col-lg-5">
+              <div className="form-group">
+                <label className="form-label">State</label>
                 <span className="form-note">
                   Specify the URL if your main website is external.
                 </span>
@@ -158,26 +216,106 @@ export default function SellerShop() {
           <div className="row g-3 align-center">
             <div className="col-lg-5">
               <div className="form-group">
-                <label className="form-label" for="site-off">
-                  Maintanance Mode
-                </label>
+                <label className="form-label">Country</label>
                 <span className="form-note">
-                  Enable to make website make offline.
+                  Specify the URL if your main website is external.
                 </span>
               </div>
             </div>
             <div className="col-lg-7">
               <div className="form-group">
-                <div className="custom-control custom-switch">
+                <div className="form-control-wrap">
                   <input
-                    type="checkbox"
-                    className="custom-control-input"
-                    name="reg-public"
-                    id="site-off"
+                    type="text"
+                    className="form-control"
+                    name="site-url"
+                    value="https://www.softnio.com"
                   />
-                  <label className="custom-control-label" for="site-off">
-                    Offline
-                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row g-3 align-center">
+            <div className="col-lg-5">
+              <div className="form-group">
+                <label className="form-label">Pincode</label>
+                <span className="form-note">
+                  Specify the URL if your main website is external.
+                </span>
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="form-group">
+                <div className="form-control-wrap">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="site-url"
+                    value="https://www.softnio.com"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row g-3">
+            <div className="col-lg-7 offset-lg-5">
+              <div className="form-group mt-2">
+                <button type="submit" className="btn btn-lg btn-primary">
+                  Update
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <hr />
+
+        <h5 className="card-title mt-5">Payment Settings</h5>
+        <p>Here are your Razorpay keys.</p>
+
+        <form action="#" className="gy-3 form-settings">
+          <div className="row g-3 align-center">
+            <div className="col-lg-5">
+              <div className="form-group">
+                <label className="form-label" for="site-copyright">
+                  City
+                </label>
+                <span className="form-note">
+                  Copyright information of your website.
+                </span>
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="form-group">
+                <div className="form-control-wrap">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="site-copyright"
+                    value="© 2019, DashLite. All Rights Reserved."
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row g-3 align-center">
+            <div className="col-lg-5">
+              <div className="form-group">
+                <label className="form-label">State</label>
+                <span className="form-note">
+                  Specify the URL if your main website is external.
+                </span>
+              </div>
+            </div>
+            <div className="col-lg-7">
+              <div className="form-group">
+                <div className="form-control-wrap">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="site-url"
+                    value="https://www.softnio.com"
+                  />
                 </div>
               </div>
             </div>
