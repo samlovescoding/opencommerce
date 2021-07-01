80b6350adb8c87b5e8b0ed3611b0c5101412d061
Sampan Verma <https://gitlab.com/sampan.rexweb/opencommerce>
2021-07-01 09:35:37 +0530

    Added customer boostrap in api

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,6 +7,7 @@ router.use("/admin", require("./admin"));
 router.use("/product", require("./product"));
 router.use("/category", require("./category"));
 router.use("/image", require("./image"));
+router.use("/customer", require("./customer"));
 router.use("/uploads", express.static(path.join("api", "uploads")));
 
 module.exports = router;
